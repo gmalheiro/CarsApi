4e2cc68a9bb5bbaf98e11d1bebbbd01ddba31932
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-09-25 23:47:50 -0300

    Fixed issues with get by id method

--- CarsAPI/Controllers/CarsController.cs
+++ CarsAPI/Controllers/CarsController.cs
@@ -1,4 +1,5 @@
 ﻿using CarsAPI.Context;
+using CarsAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,8 +16,8 @@ namespace CarsAPI.Controllers
             _context = context;
         }
 
-        [HttpGet]
-        public IActionResult Get()
+        [HttpGet("GetCars")]
+        public ActionResult <IEnumerable<Car>> Get()
         {
             var cars = _context?.Cars?.ToList();
 
@@ -26,14 +27,14 @@ namespace CarsAPI.Controllers
             return Ok(cars);
         }
 
-        [HttpGet]
-        public IActionResult GetById(int id)
+        [HttpGet("{id:int}",Name = "GetCarById")]
+        public ActionResult <Car> Get(int id)
         {
-            var car = _context?.Cars?.FirstOrDefault(c => c.CarId == id);
+            var car = _context?.Cars?.FirstOrDefault(car => car.CarId == id);
 
             if (car is null)
-                return NotFound();
-
+                return NotFound("Car not found...");
+            
             return Ok(car);
         }
     }
