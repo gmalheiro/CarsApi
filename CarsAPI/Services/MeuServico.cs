c63d0692b267ef5eab5c0a8020fb3d30e9ed6282
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-10-02 03:11:30 -0300

    MeuServico class created

---
+++ CarsAPI/Services/MeuServico.cs
@@ -0,0 +1,10 @@
+﻿namespace CarsAPI.Services
+{
+    public class MeuServico : IMeuServico
+    {
+        public string? saudacao(string nome)
+        {
+            return $"Bem vindo,{nome} \n\n {DateTime.Now}";
+        }
+    }
+}
