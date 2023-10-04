efbd4d74642728be64e89f796420c765f199c839
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-10-03 22:10:51 -0300

    Registered ApiLogging filter in program

--- CarsAPI/Program.cs
+++ CarsAPI/Program.cs
@@ -1,4 +1,5 @@
 using CarsAPI.Context;
+using CarsAPI.Filters;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,8 @@ builder.Services.AddSwaggerGen();
 string sqlConnection = builder.Configuration.GetConnectionString("DbConn");
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(sqlConnection));
+
+builder.Services.AddScoped<ApiLoggingFilter, ApiLoggingFilter>();
 
 var app = builder.Build();
 
